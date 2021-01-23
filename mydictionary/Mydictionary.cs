8f39ade6d8d282fe576bb48c6a1b787d4b7b69cb
Aynur <ay_ha@hotmail.com>
2021-01-23 20:52:02 +0300

    Proje dosyası ekle.

---
+++ mydictionary/Mydictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mydictionary
+{
+    class Mydictionary<Tkey,Tvalue>
+    {
+        Tkey[] tkey;
+        Tvalue[] tvalue;
+
+        Tkey[] temptkey;
+        Tvalue[] temptvalue;
+        public Mydictionary()
+        {
+            tkey = new Tkey[0];
+            tvalue = new Tvalue[0];
+
+        }
+
+        public void Add(Tkey id, Tvalue val) {
+            
+            temptkey = new Tkey[tkey.Length+1];
+            temptvalue = new Tvalue[tvalue.Length+1];
+            temptkey = tkey;
+            temptvalue = tvalue;
+
+            for(int i = 0; i < temptkey.Length; i++)
+            {
+              
+                tkey[i] = temptkey[i];
+                tvalue[i] = temptvalue[i];
+                tkey[tkey.Length - 1] = id;
+                tvalue[tvalue.Length - 1] = val;
+            }
+            
+            Console.WriteLine("Eklenen id=" + id  + "Eklenen isim= " + val);
+        }
+    }
+}
---
+++ mydictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace mydictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mydictionary<int, string> personelListesi = new Mydictionary<int, string>();
+            personelListesi.Add(0, "Aynur Yüksel");
+            personelListesi.Add(1, "Hakan Caymaz");
+          
+        }
+    }
+}
